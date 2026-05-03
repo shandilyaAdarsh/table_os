7b6abb1db77eca21893097badca68b89d9ce301b
vrshraj <iamvrsh@gmail.com>
2026-05-04 03:29:41 +0530

    fix: export actual customer pages instead of placeholders

--- src/apps/customer/index.jsx
+++ src/apps/customer/index.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
-export const MenuSplash = () => <div className="p-10 font-mono">Menu Splash (Public)</div>;
-export const MenuHome = () => <div className="p-10 font-mono">Menu Home / Browse (Public)</div>;
-export const ItemDetail = () => <div className="p-10 font-mono">Item Detail (Public)</div>;
-export const OrderConfirmation = () => <div className="p-10 font-mono">Order Confirmation (Public)</div>;
-export const OrderTracking = () => <div className="p-10 font-mono">Order Tracking (Public)</div>;
-export const PaymentScreen = () => <div className="p-10 font-mono">Payment Screen (Public)</div>;
+export { default as MenuSplash } from './pages/Splash';
+export { default as MenuHome } from './pages/MenuHome';
+export { default as ItemDetail } from './pages/ItemDetail';
+export { default as OrderConfirmation } from './pages/OrderConfirmed';
+export { default as OrderTracking } from './pages/OrderTracking';
+export { default as PaymentScreen } from './pages/Payment';
